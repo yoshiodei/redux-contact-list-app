6a60d417b3e5f47ab06be7a5d920a2753c843889
yoshiodei <yoshiodei@yahoo.com>
2021-09-12 14:59:14 +0000

    modify index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'; 
-import {userReducer} from './reducers/userReducer';
-
-let store = createStore(userReducer);
+import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
